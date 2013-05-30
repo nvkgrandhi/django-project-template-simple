3eed61ea20564439149344ef33c86374f71710e3
Tosh Goodson <goodsont@mit.edu>
2013-05-30 09:30:26 -0700

    Added workspace path, removed default databases.

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -51,11 +51,14 @@ MEDIA_URL = ''
 # the path to the project directory (here it's one directory up from this settings file)
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
+# the path to the workspace directory, sometimes useful to have (here it's one directory up from project)
+WORKSPACE_PATH = os.path.join(PROJECT_PATH, '..')
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- project_name/settings/test.py
+++ project_name/settings/test.py
@@ -4,10 +4,11 @@ from common import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+# TODO: add real database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'development.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
